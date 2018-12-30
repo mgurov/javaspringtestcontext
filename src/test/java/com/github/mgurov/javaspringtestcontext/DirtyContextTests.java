806c5be1d0c5b432bd339b37529e8b06cc810aad
Mykola Gurov <ngurov@gmail.com>
2018-12-30 16:30:44 +0100

    ignore DirtyContextTests

--- src/test/java/com/github/mgurov/javaspringtestcontext/DirtyContextTests.java
+++ src/test/java/com/github/mgurov/javaspringtestcontext/DirtyContextTests.java
@@ -1,5 +1,6 @@
 package com.github.mgurov.javaspringtestcontext;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Ignore //NB: disabled by default
 public class DirtyContextTests {
 
 	@Test
