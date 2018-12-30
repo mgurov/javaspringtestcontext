cba592b5cb640abef83781133e35638c4818dc2e
Mykola Gurov <ngurov@gmail.com>
2018-12-30 16:17:40 +0100

    https://start.spring.io

---
+++ src/main/java/com/github/mgurov/javaspringtestcontext/JavaspringtestcontextApplication.java
@@ -0,0 +1,14 @@
+package com.github.mgurov.javaspringtestcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringtestcontextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringtestcontextApplication.class, args);
+	}
+
+}
+
